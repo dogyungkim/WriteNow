b1f4ec5ea70d513d2feb52fb2102ef981f4e01e8
dogyungkim <prunsoli12@gmail.com>
2023-05-02 23:04:06 +0900

    Initial Commit

---
+++ WriteNow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WriteNow
+//
+//  Created by 김도경 on 2023/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WriteNow/WriteNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WriteNowApp.swift
+//  WriteNow
+//
+//  Created by 김도경 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct WriteNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
